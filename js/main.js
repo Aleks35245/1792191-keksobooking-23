29fc426da64392fe27256762cf5c7d883958bdcc
Александр Курочкин <kuras35245@gmail.com>
2021-06-16 11:59:46 +0300

    Add/Edit 2 function in file main.js - случайное целое и дробное число с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-// в качестве примера использовался материал из источника https://learn.javascript.ru/task/random-int-min-max
 function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  // задаем функцию randomInteger(min, max)
+  const rand = min + Math.random() * (max + 1 - min);
+  // находим случайное число rand - использование метода Math.random() взял из источника
+  // https://learn.javascript.ru/task/random-int-min-max
   return (min > max || min < 0) ? 'Min не может быть больше Max и меньше 0!' : Math.floor(rand);
+  // результат функции через использование тернарного оператора для выполнения условий ДЗ
+  // - положительный диапазон и минимум меньше максимума
+  // округление числа rand до целого числа методом Math.floor() - взял  из источника https://learn.javascript.ru/number
 }
-alert( randomInteger(1, 5) );
+randomInteger(); // вставил временно для проверки
 
 function randomDouble(min, max,countChar) {
-  let num = min + Math.random() * (max - min);
+  // задаем функцию randomDouble(min, max,countChar)
+  const num = min + Math.random() * (max - min);
+  // находим случайное число num - использование метода Math.random() взял из источника
+  // https://learn.javascript.ru/task/random-int-min-max
   return (min > max || min < 0) ? 'Min не может быть больше Max и меньше 0!' : +num.toFixed(countChar);
+  // результат функции через использование тернарного оператора для выполнения условий ДЗ
+  // - положительный диапазон и минимум меньше максимума
+  // округление числа num до countChar-знаков методом .toFixed() - взял  из источника https://learn.javascript.ru/number
 }
-alert( randomDouble(1, 5, 4) );
+randomDouble(); // вставил временно для проверки
