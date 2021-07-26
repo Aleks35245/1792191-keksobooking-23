8901ec203f9d675a62316dee4f027bcf55e30f01
Александр Курочкин <kuras35245@gmail.com>
2021-07-26 21:56:23 +0300

    Final#2

--- js/fetch-api.js
+++ js/fetch-api.js
@@ -10,7 +10,7 @@ const getData = (onSuccess, onError) => {
 };
 
 const sendData = (onSuccess, onError, body) => {
-  fetch('https://23.javascript.pages.academy/keksobookings', {
+  fetch('https://23.javascript.pages.academy/keksobooking', {
     method: 'POST',
     body: body,
   })
